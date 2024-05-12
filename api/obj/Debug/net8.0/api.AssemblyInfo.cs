dd1c41b6cfd9c79f2d2784614a6b87684f9615f9
SafwanSbr <safwanrahman112233@gmail.com>
2024-05-12 18:10:00 +0600

    Create Files for Token Service

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eaf2adcabc11de3c3f1e9b93f31cb68b768e026")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94975564da46a047bb670a57ee7cd549af05c705")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
