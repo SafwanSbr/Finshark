0b3e213258c9179a00ddcac9ebd7bbb27f3d8e90
SafwanSbr <safwanrahman112233@gmail.com>
2024-05-17 19:12:20 +0600

    Backend->CORS and Forntend->URL and KEY

--- frontend-react-typescript/src/Page/CompanyPage/CompanyPage.tsx
+++ frontend-react-typescript/src/Page/CompanyPage/CompanyPage.tsx
@@ -2,21 +2,54 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { CompanyProfile } from '../../company';
 import { getCompanyProfile } from '../../api';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard';
+import { error } from 'console';
+import Title from '../../Components/Title/Title';
 
-type Props = {}
+interface Props {}
 
 const CompanyPage = (props: Props) => {
   
   let {ticker} = useParams();
 
+  const tabItems = [
+    {
+      id: 1,
+      title: "Company Profile",
+      icon: "fas fa-child",
+      content: "step 1 content"
+    },
+    {
+      id: 2,
+      title: "Income Statment",
+      icon: "fas fa-users",
+      content: "step 2 content"
+    },
+    {
+      id: 3,
+      title: "Balance Sheet",
+      icon: "fas fa-network-wired",
+      content: "step 3 content"
+    },
+    {
+      id: 4,
+      title: "Cash Flow",
+      icon: "fa money-check-alt",
+      content: "step 4 content"
+    }
+  ]
+
   const [company, setCompany] = useState<CompanyProfile>();
   const [error, setError] = useState<string | null>(null);
+  const [ activeSidebarItem, setActiveSideBarItem] = useState<number>(1);
 
   useEffect(  () => {
     const getProfileInit = async () => {
       try {
         const data = await getCompanyProfile(ticker!);
         setCompany(data?.data[0]);
+        console.log(data);
       } catch (error: any) {
         if (error.response?.status === 429) {
           setError("Too many requests. Please try again later.");
@@ -32,7 +65,15 @@ const CompanyPage = (props: Props) => {
   return (
     <>
       {company ? (
-        <div>{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          
+          <Sidebar />
+          
+          <CompanyDashboard>
+            <Title title="Company Name" subTitle={company.companyName} />
+          </CompanyDashboard>
+        </div>
+
       ) : error ? (
         <div>{error}</div>
       ) : (
--- frontend-react-typescript/src/api.tsx
+++ frontend-react-typescript/src/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { CompanyProfile, CompanySearch } from "./company";
 
-const api_key = '7de6e7e5b2f29076903ea31b019f70cd';
+const api_key = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhZDEyM0BleGFtcGxlLmNvbSIsImdpdmVuX25hbWUiOiJzYWQiLCJuYmYiOjE3MTU3NTQ4OTgsImV4cCI6MTcxNjk2NDQ5OCwiaWF0IjoxNzE1NzU0ODk4LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MDg3IiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzA4NyJ9.N6rQfhWpfS3MfhIkJkcmI7uDofvCrKsZwvle8hsHz5fH3WJumVT-8ilQEbYEApLg77O9G_qEaHdsr6eYWD-84Q';
 
 export interface SearchResponse {
     data: CompanySearch[];
@@ -10,29 +10,43 @@ export interface SearchResponse {
 
 //Search 
 export const searchCompanies = async (query: string) => {
-    try{
-        const data = await axios.get<SearchResponse>(
-            `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=9&exchange=NASDAQ&apikey=${api_key}`
-          );
-          return data;
-    }catch(error){
-        if(axios.isAxiosError(error)){
-            console.log("Error Message: ", error);
-            return error.message;
+    try {
+      const data = await axios.get<SearchResponse>(
+        `https://localhost:7087/api/stock?Symbol=${query}`,
+        {
+          headers: {
+            Authorization: `Bearer ${api_key}`, // Assuming 'api_key' is your access token
+          },
         }
-        else{
-            console.log("Unexpected error: ", error);
-            return ("An Expected error occured.");
+      );
+      return data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log("Error Message: ", error);
+        // Handle specific errors (e.g., 401 for unauthorized access)
+        if (error.status === 401) {
+          console.error("Unauthorized access. Check your access token.");
         }
+        return error.message;
+      } else {
+        console.log("Unexpected error: ", error);
+        return ("An Expected error occured.");
+      }
     }
-}
+  };
+  
 
 //Get Specific Company
 export const getCompanyProfile = async (query: string) =>{
     try {
         console.log(query)
         const data = await axios.get<CompanyProfile[]>(
-            `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${api_key}`
+            `https://localhost:7087/api/stock?Symbol=${query}`,
+        {
+          headers: {
+            Authorization: `Bearer ${api_key}`, // Assuming 'api_key' is your access token
+          },
+        }
         );
         return data;
     } catch (error: any){
